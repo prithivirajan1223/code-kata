14a94926f611a6826e072e725f27d47b2b16fd59
Your Name <you@example.com>
2024-02-03 10:23:18 +0530

    test comment

--- q10.js
+++ q10.js
@@ -27,7 +27,7 @@ inp.on("close", () => {
   //start-here
   //Your code goes here … replace the below line with your code logic 
 
-  console.log(userInput[0].split('').join(','));
+  console.log(userInput[0].split('').join(','));//start logic
 
   //end-here
 });
